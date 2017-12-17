5c5ab61222c69cbf9a517d0510675e54820753b4
Anton1234567789 <toshiksokil@mail.ru>
2017-12-18 00:07:10 +0200

    part5

--- src/ua/nure/sokolov/practice6/part4/Part4.java
+++ src/ua/nure/sokolov/practice6/part4/Part4.java
@@ -1,4 +1,14 @@
 package ua.nure.sokolov.practice6.part4;
 
 public class Part4 {
+    public static void main(String[] args) {
+        UndirectedGraph graph = new UndirectedGraph();
+        graph.addVertex("1");
+        graph.addVertex("5");
+        graph.addVertex("3");
+        graph.addEdge("2","4");
+        System.out.println(graph.getVertexMap());
+        System.out.println(graph.hasVertex("6"));
+
+    }
 }
